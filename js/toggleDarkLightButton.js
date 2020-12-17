e73d90fe77df46d954c4c2f08412e5f691e86fd8
anettoph <anettoph@mail.ru>
2020-12-17 22:44:29 +0300

    rel 17.12.2020

--- js/toggleDarkLightButton.js
+++ js/toggleDarkLightButton.js
@@ -1,13 +1,14 @@
 var clickCounter = 0;
 function toggleDarkLight() {
 	let body = document.querySelector("body");
-	let buttons = document.querySelectorAll("button");
+	var buttons = document.querySelectorAll("button");
 	let currentClass = body.className;
 	let currentText = buttons[0].title;
 	body.className = currentClass == "dark-theme" ? "light-theme" : "dark-theme";
 	buttons[0].title = currentText == "Выключить свет" ? "Включить свет" : "Выключить свет";
 	clickCounter++;
 	if (clickCounter > 10) {
+		buttons[0].disabled = true;
 		clickCounter = 0;
 	}
 }
