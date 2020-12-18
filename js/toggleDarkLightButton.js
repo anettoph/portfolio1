5a552b5d31bf828fb519979400f7541d198415ce
anettoph <anettoph@mail.ru>
2020-12-18 16:07:06 +0300

    123

--- js/toggleDarkLightButton.js
+++ js/toggleDarkLightButton.js
@@ -7,8 +7,24 @@ function toggleDarkLight() {
 	body.className = currentClass == "dark-theme" ? "light-theme" : "dark-theme";
 	buttons[0].title = currentText == "Выключить свет" ? "Включить свет" : "Выключить свет";
 	clickCounter++;
-	if (clickCounter > 10) {
+	if (clickCounter > 4) {
 		buttons[0].disabled = true;
 		clickCounter = 0;
 	}
 }
+
+var upButton = document.querySelector('.up-button');
+window.onscroll = function () {
+	if (window.pageYOffset > 1200) {
+		upButton.classList.add('shown');
+		// window.scrollTo(координата - Х, координата - Y);
+	} else {
+		upButton.classList.remove('shown');
+	}
+}
+upButton.onclick = function () {
+	window.scrollTo(0, 0);
+};
+
+
+
